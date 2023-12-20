a5764de70a1b62086b0970300fe7af0907beb2fe
jcr04 <joaocarlosreboucas15@gmail.com>
2023-12-20 16:09:37 -0300

    add project

---
+++ src/main/java/com/projetos/funfa/FunfaApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.funfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunfaApplication.class, args);
+	}
+
+}
